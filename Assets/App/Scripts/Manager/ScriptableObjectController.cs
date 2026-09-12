c404411018bba051ca57bddfae8fe391c21649a4
Vuong <thinhvuong953@gmail.com>
2026-09-12 12:47:51 +0700

    Add ScriptableObject Scripts

--- Assets/App/Scripts/Manager/ScriptableObjectController.cs
+++ Assets/App/Scripts/Manager/ScriptableObjectController.cs
@@ -10,15 +10,22 @@ public class ScriptableObjectController : SingletonMono<ScriptableObjectControll
 
     [SerializeField] private UpdateBoolAction updateInteractingAction;
     [SerializeField] private UpdateBoolAction setLockCharacterAction;
+    [SerializeField] private UpdateBoolAction updateHasDishAction;
+    [SerializeField] private UpdateBoolAction updateHasMilkAction;
 
     [SerializeField] private UpdateIntAction updateInventeryFoodIndexAction;
 
     [SerializeField] private UpdateStringAction updateInventeryTextAction;
 
     [SerializeField] private VoidAction cookAction;
+    [SerializeField] private VoidAction showLoseAction;
+    [SerializeField] private VoidAction showFinishAction;
 
     [Header("Return Action")]
     [SerializeField] private ReturnBoolWithIntAction hasCollectFoodAction;
+
+    [SerializeField] private ReturnBoolAction haveDishAction;
+    [SerializeField] private ReturnBoolAction haveMilkAction;
 
     [SerializeField] private ReturnIntListAction getInventeryFoodIndexAction;
 
@@ -26,9 +33,16 @@ public class ScriptableObjectController : SingletonMono<ScriptableObjectControll
     public UpdateTextAction UpdateDateTimeTextAction => updateDateTimeTextAction;
     public UpdateBoolAction UpdateInteractingAction => updateInteractingAction;
     public UpdateBoolAction SetLockCharacterAction => setLockCharacterAction;
+    public UpdateBoolAction UpdateHasDishAction => updateHasDishAction;
+    public UpdateBoolAction UpdateHasMilkAction => updateHasMilkAction;
     public UpdateIntAction UpdateInventeryFoodIndexAction => updateInventeryFoodIndexAction;
     public ReturnBoolWithIntAction HasCollectFoodAction => hasCollectFoodAction;
     public UpdateStringAction UpdateInventeryTextAction => updateInventeryTextAction;
     public ReturnIntListAction GetInventeryFoodIndexAction => getInventeryFoodIndexAction;
     public VoidAction CookAction => cookAction;
+    public VoidAction ShowLoseAction => showLoseAction;
+    public VoidAction ShowFinishAction => showFinishAction;
+
+    public ReturnBoolAction HaveDishAction => haveDishAction;
+    public ReturnBoolAction HaveMilkAction => haveMilkAction;
 }
